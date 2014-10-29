98a6b07f3ecec90953d81817c35261f516bd48ef
yangzhanguo120 <zhanguo120>
2014-10-29 10:14:31 +0800

    dafadsfdsafdsafds

---
+++ js/controller.js
@@ -0,0 +1,10 @@
+function PhoneListCtrl($scope) {
+  $scope.phones = [
+    {"name": " 诺基亚",
+     "snippet": "Fast just got faster with Nexus S."},
+    {"name": "苹果",
+     "snippet": "The Next, Next Generation tablet."},
+    {"name": "摩托罗拉",
+     "snippet": "The Next, Next Generation tablet."}
+  ];
+}
